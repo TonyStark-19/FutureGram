19cf3c96746faeeb3ebf6697f8ae7a6cc9a485d8
GeniusPrarthana <Prarthanagade25@gmail.com>
2025-07-07 15:08:58 +0200

    Added db.js config

--- backend/models/letter.js
+++ backend/models/letter.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose");
 
 const letterSchema = new mongoose.Schema({
   email: { type: String, required: true },
+
   content: { type: String, required: true },
+
   scheduledDate: { type: Date, required: true },
+  
   sent: { type: Boolean, default: false },
 });
 
