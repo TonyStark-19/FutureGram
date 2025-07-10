7652f76754dcad675bcae9de3814be2b6f3be759
GeniusPrarthana <Prarthanagade25@gmail.com>
2025-07-10 19:11:57 +0200

    fixing api issues in frontend

--- frontend/src/Api.jsx
+++ frontend/src/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // base URL of your backend
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api",
 });
 
 export default API;
