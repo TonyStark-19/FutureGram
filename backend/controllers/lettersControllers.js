1e46dc0c3a7896fa2a66ed14a2c939f093070ab3
GeniusPrarthana <Prarthanagade25@gmail.com>
2025-07-13 07:03:01 +0200

    Made changes in the backend

--- backend/controllers/lettersControllers.js
+++ backend/controllers/lettersControllers.js
@@ -1,33 +1,25 @@
-const cloudinary = require("../utils/cloudinary");
 const Letter = require("../models/letter");
 
 exports.createLetter = async (req, res) => {
-  try {
-    const { name, email, content, scheduledDate, animeName } = req.body;
+    try {
+        const { name, email, content, scheduledDate, animeName, animePicUrl } = req.body;
 
-    let animePic = req.body.animePic || "https://i.ibb.co/BBRpLkx/default-anime.jpg";
-    let animeNameFinal = animeName || "Hitori";
+        const animePic = animePicUrl || "https://i.ibb.co/BBRpLkx/default-anime.jpg";
+        const animeNameFinal = animeName || "Hitori";
 
-    // If image is uploaded
-    if (req.file) {
-      const result = await cloudinary.uploader.upload(req.file.path);
-      animePic = result.secure_url;
-      animeNameFinal = animeName || "Custom Anime";
+        const letter = new Letter({
+            name,
+            email,
+            content,
+            scheduledDate,
+            animePic,
+            animeName: animeNameFinal,
+        });
+
+        await letter.save();
+        res.status(201).json(letter);
+    } catch (err) {
+        console.error("Letter creation error:", err);
+        res.status(500).json({ message: "Server error", error: err });
     }
-
-    const letter = new Letter({
-      name,
-      email,
-      content,
-      scheduledDate,
-      animePic,
-      animeName: animeNameFinal,
-    });
-
-    await letter.save();
-    res.status(201).json(letter);
-  } catch (err) {
-    console.error("Upload error:", err);
-    res.status(500).json({ message: "Server error", error: err });
-  }
 };
--- backend/models/letter.js
+++ backend/models/letter.js
@@ -1,33 +1,12 @@
 const mongoose = require("mongoose");
 
 const letterSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    default: "Mystery Writer",
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  scheduledDate: {
-    type: Date,
-    required: true,
-  },
-  animePic: {
-  type: String,
-  default: "https://i.ibb.co/BBRpLkx/default-anime.jpg",
-},
-animeName: {
-  type: String,
-},
-  sent: {
-    type: Boolean,
-    default: false,
-  },
-});
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    content: { type: String, required: true },
+    scheduledDate: { type: Date, required: true },
+    animePic: { type: String, default: "https://i.ibb.co/BBRpLkx/default-anime.jpg" },
+    animeName: { type: String, default: "Hitori" },
+}, { timestamps: true });
 
 module.exports = mongoose.model("Letter", letterSchema);
